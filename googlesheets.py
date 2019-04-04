6d9f7782d166ae9ec98bfe653d2f67066d354da3
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2019-04-05 00:35:10 +0100

    Cenas

--- googlesheets.py
+++ googlesheets.py
@@ -10,9 +10,14 @@ from google.auth.transport.requests import Request
 from competitors import competitor
 from challengecs import challengec
 import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+#SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+scope = ['https://spreadsheets.google.com/feeds']
+
+client = gspread.authorize(creds)
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1YR4zEnhQM9JJRNoBh7q_tkmBpLDaz80uYPEOzYi94s8'
@@ -24,24 +29,25 @@ class googlesheets:
 		"""Shows basic usage of the Sheets API.
 		Prints values from a sample spreadsheet.
 		"""
-		creds = None
+		#creds = None
+		creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 		# The file token.pickle stores the user's access and refresh tokens, and is
 		# created automatically when the authorization flow completes for the first
 		# time.
-		if os.path.exists('token.pickle'):
-			with open('token.pickle', 'rb') as token:
-				creds = pickle.load(token)
-		# If there are no (valid) credentials available, let the user log in.
-		if not creds or not creds.valid:
-			if creds and creds.expired and creds.refresh_token:
-				creds.refresh(Request())
-			else:
-				flow = InstalledAppFlow.from_client_secrets_file(
-					'credentials.json', SCOPES)
-				creds = flow.run_local_server()
-			# Save the credentials for the next run
-			with open('token.pickle', 'wb') as token:
-				pickle.dump(creds, token)
+		# if os.path.exists('token.pickle'):
+			# with open('token.pickle', 'rb') as token:
+				# creds = pickle.load(token)
+		# # If there are no (valid) credentials available, let the user log in.
+		# if not creds or not creds.valid:
+			# if creds and creds.expired and creds.refresh_token:
+				# creds.refresh(Request())
+			# else:
+				# flow = InstalledAppFlow.from_client_secrets_file(
+					# 'credentials.json', SCOPES)
+				# creds = flow.run_local_server()
+			# # Save the credentials for the next run
+			# with open('token.pickle', 'wb') as token:
+				# pickle.dump(creds, token)
 
 		self.service = build('sheets', 'v4', credentials=creds)
 
